2aa850922637cbe34f0bb3c9814bd943dd8bbf3e
mdansarulislam641 <mdansarulislam641.com>
2023-06-20 11:56:06 +0600

    btn color change:

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -16,7 +16,7 @@ const Banner = () => {
       <div className="max-w-screen-xl h-full mx-auto flex justify-between items-center">
         <div className='flex-1 data-aos="fade-left" data-aos-id="super-duper"  rounded-3xl  flex items-center justify-center'>
           <div className="">
-            <h3 className="uppercase text-2xl md:text-3xl lg:text-5xl text-center font-extrabold font-mono text text-amber-600 px-5">
+            <h3 className="uppercase text-2xl md:text-3xl lg:text-5xl text-center font-extrabold font-mono text text-white px-5">
               I'm a <span className="text-teal-900">{text}</span>
               <Cursor cursorColor="red" />
             </h3>
@@ -25,10 +25,10 @@ const Banner = () => {
               HTML | CSS | JavaScript | React | NodeJs
             </h4>
             <div className="text-center flex gap-x-5 justify-center py-3">
-              <button className="btn border-2 text-white hover:translate-x-0 hover:border-red-700 bg-red-400 border-red-700 btn-ghost bg-transparent">
+              <button className="btn border-2 text-white bg-teal-800 border-3">
                 <a href="#projects">Projects</a>
               </button>
-              <button className="btn border-2 text-white hover:translate-x-0 hover:border-yellow-700 hover:text-amber-800  border-red-700 btn-ghost bg-transparent">
+              <button className="btn border-2 text-white bg-teal-800 border-3">
                 {" "}
                 <a href={resume} download="portfolio" target="_blank" rel="noopener noreferrer">
                   REsume
@@ -36,14 +36,14 @@ const Banner = () => {
               </button>
             </div>
             {/* social icons  */}
-            <div className="flex justify-center gap-5 pt-2">
-              <a href="https://www.facebook.com/md.ansarulislam.641" target="_blank" rel="noopener noreferrer"> <BsFacebook size={30} className=" text-blue-800 cursor-pointer" /></a>
+            <div className="flex justify-center gap-5 pt-2 z-40">
+              <a href="https://www.facebook.com/md.ansarulislam.641" target="_blank" rel="noopener noreferrer"> <BsFacebook size={30} className=" text-blue-900 cursor-pointer" /></a>
              
               <a href="https://www.linkedin.com/in/md-ansarul-haque/" target="_blank" rel="noopener noreferrer">
-              <BsLinkedin size={30} className=" text-indigo-700 cursor-pointer"/>
+              <BsLinkedin size={30} className=" text-indigo-200 cursor-pointer"/>
               </a>
               <a href="https://github.com/mdansarulislam641" target="_blank" rel="noopener noreferrer">
-              <BsGithub size={30} className=" text-indigo-700 cursor-pointer"/>
+              <BsGithub size={30} className=" text-gray-700 cursor-pointer"/>
               </a>
               
             </div>
